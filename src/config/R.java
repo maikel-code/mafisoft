cd66497144973ee90ff4ecce33ea5bf1f4776a4a
MrPitty <yermash.peter@gmail.com>
2017-06-24 01:32:15 +0200

    init config

---
+++ src/config/R.java
@@ -0,0 +1,19 @@
+package config;
+
+/*
+  Created by mrpitty on 20.06.17.
+ */
+
+public class R {
+    public static class Log {
+        public static final String LOG_PROPERTIES_FILE = "logs/log.properties";
+    }
+
+    public static class DB {
+        public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    }
+
+    public static class Home {
+        public static final String PATH_TO_MAINWINDOW = "gui/Homepage.fxml";
+    }
+}
