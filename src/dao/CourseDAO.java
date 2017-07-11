aaf593acaaa7dc94884042984613f19d98ef548f
MrPitty <yermash.peter@gmail.com>
2017-07-11 10:36:07 +0200

    remove throws from interface

--- src/dao/CourseDAO.java
+++ src/dao/CourseDAO.java
@@ -6,32 +6,30 @@ import dto.courses.VideoCourse;
 import dto.customer.Customer;
 import javafx.collections.ObservableList;
 
-import java.sql.SQLException;
-
 public interface CourseDAO {
 
-    ObservableList<PhysicalCourse> getAllCourseByCustomer(Customer dtoCustomer) throws SQLException;
+    ObservableList<PhysicalCourse> getAllCourseByCustomer(Customer dtoCustomer);
 
-    int addPhysicalCourse(PhysicalCourse dtoPhysicalCourse) throws SQLException;
+    int addPhysicalCourse(PhysicalCourse dtoPhysicalCourse);
 
-    void updatePhysicalCourse(PhysicalCourse dtoPhysicalCourse) throws SQLException;
+    void updatePhysicalCourse(PhysicalCourse dtoPhysicalCourse);
 
-    ObservableList<PhysicalCourse> searchPhysicalCourse(String searchConfig, String search) throws SQLException;
+    ObservableList<PhysicalCourse> searchPhysicalCourse(String searchConfig, String search);
 
-    ObservableList<PhysicalCourse> getAllCourse() throws SQLException ;
+    ObservableList<PhysicalCourse> getAllCourse();
 
-    ObservableList<PhysicalCourse> getAllAvailabileCourse(Customer dtoCustomer) throws SQLException;
+    ObservableList<PhysicalCourse> getAllAvailabileCourse(Customer dtoCustomer);
 
-    void addCourseToCustomer(Customer dtoCustomer, PhysicalCourse dtoCourse) throws SQLException;
+    void addCourseToCustomer(Customer dtoCustomer, PhysicalCourse dtoCourse);
 
-    void removeCourseByCustomer(Customer dtoCustomer, PhysicalCourse dtoCourse) throws SQLException;
+    void removeCourseByCustomer(Customer dtoCustomer, PhysicalCourse dtoCourse);
 
-    int addVideoCourse(VideoCourse dtoVideoCourse) throws SQLException;
+    int addVideoCourse(VideoCourse dtoVideoCourse);
 
-    void updateVideoCourse(VideoCourse dtoVideoCourse) throws SQLException;
+    void updateVideoCourse(VideoCourse dtoVideoCourse);
 
-    ObservableList<VideoCourse> searchVideoCourse(String searchConfig, String search) throws SQLException;
+    ObservableList<VideoCourse> searchVideoCourse(String searchConfig, String search);
 
-    ObservableList<VideoCourse> getAllVideoCourse() throws SQLException;
+    ObservableList<VideoCourse> getAllVideoCourse();
 
 }
--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -3,17 +3,15 @@ package dao;
 import dto.customer.Customer;
 import javafx.collections.ObservableList;
 
-import java.sql.SQLException;
-
 public interface CustomerDAO {
 
-    int addCustomer(Customer customer) throws SQLException;
+    int addCustomer(Customer customer) ;
 
-    void updateCustomer(Customer updateCustomer) throws SQLException;
+    void updateCustomer(Customer updateCustomer) ;
 
-    ObservableList<Customer> searchCustomer(String searchConfig, String search) throws SQLException;
+    ObservableList<Customer> searchCustomer(String searchConfig, String search) ;
 
-    ObservableList<Customer> getAllCustomer() throws SQLException;
+    ObservableList<Customer> getAllCustomer() ;
 
     void removeCustomerById(int id);
 
