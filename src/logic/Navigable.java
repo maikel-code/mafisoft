ed64a22a86ba4fd6d695dd73081b0469a61e808f
MrPitty <yermash.peter@gmail.com>
2017-07-14 00:49:11 +0200

    Add JavaDoc description

--- src/logic/Navigable.java
+++ src/logic/Navigable.java
@@ -1,5 +1,6 @@
 package logic;
 
+import com.sun.istack.internal.NotNull;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -11,9 +12,21 @@ import java.io.IOException;
 import java.util.ResourceBundle;
 
 public interface Navigable {
+    /**
+     * <b>Navigate between windows</b>
+     * <br>
+     * Any class that implements or interface that extended the interface Navigable will do not have to implement the method goToScene,
+     * <br>
+     * this is <u>already implemented</u>, but asking for 2 param
+     *
+     * @param actionEvent Requested Action Event from the current window
+     * @param path        Requested path to the required page
+     * @since JDK 1.8 because of <code>default</code> method
+     */
+
     default void goToScene(ActionEvent actionEvent, String path) {
         try {
-            Parent home_page_parent = FXMLLoader.load(getClass().getClassLoader().getResource(path));
+            @NotNull Parent home_page_parent = FXMLLoader.load(getClass().getClassLoader().getResource(path));
             Scene home_page_scene = new Scene(home_page_parent);
             Stage app_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             app_stage.setScene(home_page_scene);
@@ -24,4 +37,5 @@ public interface Navigable {
     }
 
     void setResources(ResourceBundle resourceBundle);
+
 }
