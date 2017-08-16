d91a50ea2d0a0e8011de02d5830eabdd2246032d
MrPitty <yermash.peter@gmail.com>
2017-08-16 11:48:30 +0200

    fix bug with logs // checkout log directories

--- src/Mafisoft.java
+++ src/Mafisoft.java
@@ -5,12 +5,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Mafisoft extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        checkLogs();
         launch(args);
     }
 
@@ -25,6 +31,18 @@ public class Mafisoft extends Application {
         Parent root = FXMLLoader.load(getClass().getResource(R.Pages.PATH_TO_MAIN_WINDOW));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    private static void checkLogs() {
+        Path path = Paths.get("../logs");
+        System.out.println(path);
+        if (!Files.exists(path)) {
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
